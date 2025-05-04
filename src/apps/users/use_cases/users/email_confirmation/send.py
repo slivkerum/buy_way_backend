2e8e30880f32946f135de917e31c61bdb9d3e769
dima <slivkerum@mail.ru>
2025-05-05 03:40:21 +0500

    added/<html send>

--- src/apps/users/use_cases/users/email_confirmation/send.py
+++ src/apps/users/use_cases/users/email_confirmation/send.py
@@ -1,6 +1,9 @@
 from datetime import timedelta
 from django.utils import timezone
+from django.utils.html import strip_tags
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
 from apps.users.models.email_confirmation import EmailConfirmationCode
 from apps.users.models import User
 
@@ -20,9 +23,22 @@ class SendEmailConfirmationCodeUseCase:
             expires_at=expires_at
         )
 
-        send_mail(
-            subject="Активация аккаунта",
-            message=f"Ваш код подтверждения: {code}",
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[email]
+        self.send_confirmation_email(user, code)
+
+    @staticmethod
+    def send_confirmation_email(user: User, code: str) -> None:
+        subject = "Подтверждение email"
+        html_message = render_to_string(
+            'send.html',
+            {'user': user, 'confirmation_code': code}
         )
+        plain_message = strip_tags(html_message)
+
+        send_mail(
+            subject,
+            plain_message,
+            EMAIL_HOST_USER,
+            [user.email],
+            html_message=html_message
+        )
+
